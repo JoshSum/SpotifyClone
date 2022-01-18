47069ee87aed509b686097de7a8abd3741d1f686
JoshSum <joshuakuadrat@gmail.com>
2022-01-18 21:06:25 +0700

    🔥 Finally Do the Login Stuff

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 function Login({ providers }) {
     return (
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-            <Image className="w-52 mb-5" src="/spotify-logo.png" alt="me" width="150" height="150" />
+            <Image className="w-52 mb-5" src="/spotify-logo.png" alt="me" width="150" height="150" layout="fixed" />
             {/* <img className="w-52 mb-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="spotify-logo" /> */}
             {/* <img className="w-52 mb-5" src="../public/spotify.jpg" alt="spotify-logo" /> */}
             {/* <button className="bg-[#18D860] text-white p-5 rounded-lg">TEST</button> */}
@@ -11,7 +11,7 @@ function Login({ providers }) {
                 return (
                 <div key={provider.name}>
                     <button className="bg-[#18D860] text-white p-5 rounded-lg"
-                    onClick={() => signIn(provider.id, {callbackUrl: "/"})}
+                    onClick={() => signIn(provider.id, { callbackUrl: "http://localhost:3000/" })}
                     >
                         Login with {provider.name}
                     </button>
