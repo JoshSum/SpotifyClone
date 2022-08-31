40d6d549ae25c1ccb154c7cc75784f314dff2a10
JoshSum <joshuakuadrat@gmail.com>
2022-09-01 00:34:36 +0700

    ⚡️ Fix Fatal Error In PROD

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,32 @@ import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+
+// ignore in-browser next/js recoil warnings until its fixed.
+const mutedConsole = memoize((console) => ({
+  ...console,
+  warn: (...args) => args[0].includes('Duplicate atom key')
+    ? null
+    : console.warn(...args)
+}))
+
+global.console = mutedConsole(global.console);
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
